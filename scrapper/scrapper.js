1aa31fce9a2c2e7941c29e2f1f306eb7d372cd11
ArtemeeSenin <artemee.senin@gmail.com>
2017-12-29 17:12:23 +0300

    fixing plist auth

--- scrapper/scrapper.js
+++ scrapper/scrapper.js
@@ -7,11 +7,13 @@ const USERDATA = JSON.parse(fs.readFileSync('userdata.txt'));
 
 osmosis
     .get(URL)
+    .delay(1)
     .follow('a[href="\/users\/sign_in"]@href')
     .submit('#new_user', {
       'user[email]': USERDATA.login,
       'user[password]': USERDATA.password
     })
+    .delay(1)
     .set({'content': '.js-react-on-rails-component'})
     .data(function(data){
 
