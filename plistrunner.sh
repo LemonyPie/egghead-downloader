e3086e2227462766409c14175ae592a071c7cf86
ArtemeeSenin <artemee.senin@gmail.com>
2017-12-29 16:25:28 +0300

    added simple logging

--- plistrunner.sh
+++ plistrunner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+rm -rf "log.txt"
 
 while read TASK
 do
   echo -e "\033[37;1;41m starting new task $TASK \033[0m"
   . taskrunner.sh $TASK
-done < plist.txt
+done < plist.txt | tee log.txt
